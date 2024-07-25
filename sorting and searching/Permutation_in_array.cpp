66b9f23b617a1d85589e3c0f99987c5146da34f6
Aman Tripathi <amanttripathi02@gmail.com>
2024-07-25 21:57:20 +0530

    linked list solutions

---
+++ sorting and searching/Permutation_in_array.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    bool isPossible(long long a[], long long b[], int n, long long k)
+    {
+        // Your code goes here
+
+        sort(a, a + n);
+
+        // Sort the array b[] in increasing order.
+        sort(b, b + n, greater<long long>());
+
+        // Checking condition on each index.
+        for (int i = 0; i < n; i++)
+            if (a[i] + b[i] < k)
+                return false;
+
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        long long k;
+        cin >> n >> k;
+        long long a[n + 2], b[n + 2];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        for (int i = 0; i < n; i++)
+            cin >> b[i];
+        Solution ob;
+        cout << ob.isPossible(a, b, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
